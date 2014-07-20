8dd0674eec34726996642f77a35feb6be3128d51
Stefan Scherer <scherer_stefan@icloud.com>
2014-07-20 15:38:21 +0200

    fixed LRTimelapse installation

--- scripts/install-lrtimelapse.sh
+++ scripts/install-lrtimelapse.sh
@@ -9,6 +9,10 @@ if [ ! -d "/Applications/LRTimelapse 3.app/" ]; then
   mkdir -p "/Applications/LRTimelapse 3.app/"
   sudo cp -r "LRTimelapse 3.app" "/Applications/"
   sudo installer -pkg "LRTimelapse 3.4 Addons.mpkg" -target /
+  # run postflight for current user
+  "LRTimelapse 3.4 Addons.mpkg/Contents/Packages/lightroom.pkg/Contents/Resources/postflight"
+  # written as root, so we have to sudo to cleanup
+  sudo rm -rf /temp/LRTExport
   popd
   sleep 5
   hdiutil detach /dev/disk1
